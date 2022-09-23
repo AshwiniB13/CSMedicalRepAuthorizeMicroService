7ce512a4bcd5ad1c908fd0f9b6b3914fb4ec5ab8
ashwini_basavaraju <ashwini_basavaraju@cms.co.in>
2022-09-23 15:22:00 +0530

    commit

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,57 @@
+﻿using CSMedicalRepAuthorizeMicroService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMedicalRepAuthorizeMicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+
+        public AuthorizationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login()
+        {
+            IActionResult response = Unauthorized();
+            {
+                var tokenString = GenerateJSONWebToken();
+                response = Ok(new TokenResponse { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken()
+        {
+
+            List<Claim> claims = new List<Claim>();
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(2),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Models/TokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSMedicalRepAuthorizeMicroService.Models
+{
+    public class TokenResponse
+    {        public string token { get; set; }
+    }
+}
